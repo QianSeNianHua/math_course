7dad5ff5f1b268dd3108c69bb69e298074019542
Zhengliwan <870249040@qq.com>
2019-01-16 13:56:27 +0800

    zlw update

--- admin/logout.php
+++ admin/logout.php
@@ -1,15 +1,23 @@
 <?php
 //退出登录
 header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求  
-class logout{
+require_once(realpath(dirname(__FILE__) . "/../") . "/common/connection.php");
+error_reporting(0);
+class Logout{
+	private $conn;
+    /*
+    *  构造函数，建立数据库连接
+    */
+    public function __construct() {
+        $this->conn = new Connection();
+    }
 	public function logout(){
-		header("content-type:text/html;charset=utf-8");
 	    if(!empty($_COOKIE["username"])){
 			setcookie("username",'null',time()-3600,'/');
-			echo json_encode(array('status'=>true));
+			echo  json_encode(array('status'=>true));//返回json格式数据
 		}else{
-			echo json_encode(array('status'=>false));
-			}
+			echo  json_encode(array('status'=>false));//返回json格式数据
+		}
 	}
 }
- ?>
+?>
