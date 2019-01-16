7e25bdbd81499ec82864dd6f1167a6fae2d48019
Dengjunle <46371556+Dengjunle@users.noreply.github.com>
2019-01-16 13:11:17 +0800

    djl:update

--- admin/logout.php
+++ admin/logout.php
@@ -1,16 +1,15 @@
 <?php
 //退出登录
 header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求  
-function logout(){
-	header("content-type:text/html;charset=utf-8");
-    if(!empty($_COOKIE["username"])){
-		setcookie("username",'null',time()-3600,'/');
-		echo "<script>alert('退出成功')</script>";
-		header("location:html/adminLogin.html");
-	}else{
-		echo "<script>alert('退出失败')</script>";
-		header("location:html/index1.html");
-		}
+class logout{
+	public function logout(){
+		header("content-type:text/html;charset=utf-8");
+	    if(!empty($_COOKIE["username"])){
+			setcookie("username",'null',time()-3600,'/');
+			echo json_encode(array('status'=>true));
+		}else{
+			echo json_encode(array('status'=>false));
+			}
+	}
 }
-echo logout();
  ?>
